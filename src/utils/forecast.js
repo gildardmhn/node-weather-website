8b61286c45f261f48e0782052bae9ad05b01c30e
GildardMyehouenouOSF <gildard.myehouenou@osf-global.com>
2019-09-03 16:08:27 -0300

    Updated photo and lang

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,14 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/0987539c3e63dd113e9c5b9268d66d32/' + latitude + ',' + longitude + '?units=si&lang=fr'
+    const url = 'https://api.darksky.net/forecast/0987539c3e63dd113e9c5b9268d66d32/' + latitude + ',' + longitude + '?units=si'
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to the server', undefined)
         } else if (body.error) {
             callback('Unable to find this location. Try another', undefined)
         } else {
-            callback(undefined, body.currently.summary + ' .Il fait actuellement ' + body.currently.temperature + ' degré dehors. il y a ' + body.currently.precipProbability + ' % de chance de pluie')
+            callback(undefined, body.currently.summary + ' .It is currently ' + body.currently.temperature + ' degrees out. There is ' + body.currently.precipProbability + ' % chance of rain')
         }
     })
 }
