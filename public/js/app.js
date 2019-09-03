db732a2b5991cb50c2782deb281e5ea779e01daa
GildardMyehouenouOSF <gildard.myehouenou@osf-global.com>
2019-09-03 15:20:53 -0300

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client Side Js is Loaded')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#messageOne')
