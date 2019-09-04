fab25faf16ca1d8dafdcf947131fb2dc1e665051
GildardMyehouenouOSF <gildard.myehouenou@osf-global.com>
2019-09-04 14:00:57 -0300

    Updated Index page title and edited the description of the forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find this location. Try another', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is ' + body.currently.precipProbability + ' % chance of rain. The min temperature is ' + body.daily.data[0].temperatureMin + ' and the max temperature is ' + body.daily.data[0].temperatureMax)
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is ' + body.currently.precipProbability + ' % chance of rain. The min temperature is ' + body.daily.data[0].temperatureMin + ' degrees and the max temperature is ' + body.daily.data[0].temperatureMax + ' degrees.')
         }
     })
 }
